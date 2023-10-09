80c9a148b3cac18be14ee09c2ae877100308fb88
amirtechnoide <amirzoutor@gmail.com>
2023-10-09 11:03:34 +0100

    removed required for video

--- src/modules/@dashboard/blog/utils/validation.js
+++ src/modules/@dashboard/blog/utils/validation.js
@@ -7,9 +7,12 @@ const blogValidation = Yup.object().shape({
     rubrique: Yup.object().required('Sélectionnez une rubrique'),
     tag: Yup.object().required('Sélectionnez un tag'),
     link: Yup.string().required("Entrez l'URL du média"),
-    description: Yup.string().required("Entrez une description de l'article"),
+    // description: Yup.string().required("Entrez une description de l'article"),
     cover_image: Yup.string().when('type', (type, schema) => {
         return type[0] === 'video' ? schema.required("Entrez une URL de couverture") : schema;
+    }),
+    description: Yup.string().when('type', (type, schema) => {
+        return type[0] !== 'video' ? schema.required("Entrez une description de l'article") : schema;
     }),
     location: Yup.string().when('type', (type, schema) => {
         return type[0] === 'evenements' ? schema.required('Entrez une location') : schema;
@@ -28,7 +31,7 @@ const youtubeVideoRegex = /(?:\?v=|&v=|\/embed\/|\.be\/)([\w\d_-]+)/
 const checkUpladedFile = (file) => {
     const extension = file?.type?.split('/')[0]
     let validFile = true
-    
+
     if (extension !== 'image') {
         validFile = false
         toast.error('Veuillez choisir un fichier image !', {
